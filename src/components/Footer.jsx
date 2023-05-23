20590775b6b3977d0facee332c013ef49994a55c
CMLTTN <cemaldim@gmail.com>
2023-05-23 14:00:53 +0300

    all

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import FooterStyle from "../scss/footer.module.scss";
+
+
+const Footer = () => {
+  return (
+
+     <footer className={FooterStyle.footer} >
+          <p>Copyright by Clarusway {new Date().getFullYear()}</p>
+     </footer>
+  )
+
+  
+}
+
+export default Footer
